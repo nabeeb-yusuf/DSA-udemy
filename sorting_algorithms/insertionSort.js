46cce53bf053e6b71303107c7c25c9b2610d72fe
Nabeeb Yusuf <nabeeb2000@gmail.com>
2024-11-27 20:32:23 +1100

    refactor insertionSort.js

--- sorting_algorithms/insertionSort.js
+++ sorting_algorithms/insertionSort.js
@@ -1,13 +1,11 @@
 function insertionSort(arr) {
     for (let i = 1; i <arr.length; i++) {
         let currentVal = arr[i];
-        /**
-         * I have to use var j instead of let j so that j is still usable outside of the loop
-         */
-        for (var j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
+        let j; // Declare j here so it is usable outside the loop
+        for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
             arr[j + 1] = arr[j];
         }
-        arr[j + 1] = currentVal; // var j makes it possible to use j outside of the loop
+        arr[j + 1] = currentVal; // accessing j outside the loop
     }
     return arr;
 }
