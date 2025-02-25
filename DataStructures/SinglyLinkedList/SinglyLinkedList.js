26bb0107f8d65e8338489194d5034c8ca3dbdb8d
Nabeeb Yusuf <nabeeb2000@gmail.com>
2025-02-26 00:39:29 +1100

    implement get,set,insert,remove and reverse

--- DataStructures/SinglyLinkedList/SinglyLinkedList.js
+++ DataStructures/SinglyLinkedList/SinglyLinkedList.js
@@ -86,11 +86,81 @@ class SinglyLinkedList {
         if (!this.head) { // If empty list
             this.head = newNode;
             this.tail = this.head;
-            this.length += 1;
         } else {
             newNode.next = this.head;
             this.head = newNode;
-            this.length += 1;
+        }
+
+        this.length += 1;
+
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) return null;
+
+        let counter = 0;
+        let current = this.head;
+
+        while (counter !== index) {
+            current = current.next;
+            counter += 1;
+        }
+        return current;
+    }
+
+    set(index, value) {
+        let foundNode = this.get(index);
+
+        if (foundNode) {
+            foundNode.val = value;
+            return true;
+        }
+        return false;
+    }
+
+    insert(index, value) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length) return !!this.push(value);
+
+        let newNode = new Node(value);
+        let prev = this.get(index - 1);
+        let temp = prev.next;
+
+        prev.next = newNode;
+        newNode.next = temp;
+        this.length += 1;
+
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index >= this.length) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        let prev = this.get(index - 1);
+        let removed = prev.next;
+        prev.next = removed.next;
+        this.length -= 1;
+
+        return removed;
+    }
+
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node;
+
+        let next;
+        let prev = null;
+
+        for (let i = 0; i < this.length; i++) {
+            next = node.next;
+            node.next = prev;
+            prev = node;
+            node = next;
         }
 
         return this;
