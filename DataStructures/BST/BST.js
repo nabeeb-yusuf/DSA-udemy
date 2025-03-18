0dfc90a126856e462fcdb598137cc5391dacd2f3
Nabeeb Yusuf <nabeeb2000@gmail.com>
2025-03-18 18:49:58 +1100

    implement bfs & dfs

--- DataStructures/BST/BST.js
+++ DataStructures/BST/BST.js
@@ -54,4 +54,60 @@ class BinarySearchTree {
         if (!found) return undefined;
         return current;
     }
+
+    bfs() {
+        let data = [],
+            queue = [],
+            node = this.root;
+
+        queue.push(this.root);
+
+        while (queue.length) {
+            node = queue.shift();
+            data.push(node.value);
+            if (node.left) queue.push(node.left);
+            if (node.right) queue.push(node.right);
+
+        }
+        return data;
+    }
+
+    dfsPreOrder() {
+        let data = [];
+
+        function traverse(node){
+            data.push(node.value);
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsPostOrder() {
+        let data = [];
+
+        function traverse(node){
+            if (node.left) traverse(node.left);
+            if (node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+
+    dfsInOrder() {
+        let data = [];
+
+        function traverse(node){
+            if (node.left) traverse(node.left);
+            data.push(node.value);
+            if (node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
 }
